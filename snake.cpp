544a8fc1fdee92aa3db583b4baea487d9611d8ef
b2utyyomi <JG20131426@163.com>
2017-04-11 18:37:31 +0800

    first commit

---
+++ snake.cpp
@@ -0,0 +1,15 @@
+/*******************************
+author: yomi
+date: 17.1.
+mood:
+ps:
+*******************************/
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
